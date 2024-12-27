af61446c5d91ed92bdede3a5e19876a3db87b924
Ayoub Rached <Ayoub@mac.mynetworksettings.com>
2024-12-27 00:18:40 -0500

    Designed home page, added font, added native wind compatibility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
+import "../global.css";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    PoppinsSemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="quran" options={{ title: "Quran" }} />
